9e8db89fd3ebb539891eed07920a268131d2ecd0
Magitoneu <mtoneupanicot@gmail.com>
2017-05-15 20:06:02 +0200

    Deleting old shit

--- robolang/src/main/java/com/robolang/Common.java
+++ robolang/src/main/java/com/robolang/Common.java
@@ -1,5 +1,6 @@
 package com.robolang;
 
+import lejos.nxt.*;
 
 public class Common {
 
@@ -9,6 +10,34 @@ public class Common {
 
     public static void print(double val){
         System.out.println(val);
+    }
+
+    public static void move_front(double units) {
+        NXTRegulatedMotor A = new NXTRegulatedMotor(MotorPort.A);
+        NXTRegulatedMotor B = new NXTRegulatedMotor(MotorPort.B);
+        A.rotate((int) (units * 360));
+        B.rotate((int) (units * 360));
+    }
+
+    public static void move_back(double units) {
+        NXTRegulatedMotor A = new NXTRegulatedMotor(MotorPort.A);
+        NXTRegulatedMotor B = new NXTRegulatedMotor(MotorPort.B);
+        A.rotate((int) (-units * 360));
+        B.rotate((int) (-units * 360));
+    }
+
+    public static void rotate_left(int degrees) {
+        NXTRegulatedMotor A = new NXTRegulatedMotor(MotorPort.A);
+        NXTRegulatedMotor B = new NXTRegulatedMotor(MotorPort.B);
+        A.rotate(degrees);
+        B.rotate(-degrees);
+    }
+
+    public static void rotate_right(int degrees) {
+        NXTRegulatedMotor A = new NXTRegulatedMotor(MotorPort.A);
+        NXTRegulatedMotor B = new NXTRegulatedMotor(MotorPort.B);
+        A.rotate(-degrees);
+        B.rotate(degrees);
     }
 
 }
