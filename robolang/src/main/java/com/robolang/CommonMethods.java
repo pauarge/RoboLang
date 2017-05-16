b15181d38d54160cda4a2400e9e6f00d574479ca
vicmassy <vicmassy5@gmail.com>
2017-05-16 19:25:28 +0200

    More functions added

--- robolang/src/main/java/com/robolang/CommonMethods.java
+++ robolang/src/main/java/com/robolang/CommonMethods.java
@@ -1,5 +1,5 @@
 package com.robolang;
 
 public enum CommonMethods {
-    print, move_front, move_back, rotate_left, rotate_right, explore, move, waitToBePressed, followLine
+    print, move_front, move_back, rotate_left, rotate_right, explore, move, waitToBePressed, followLine, shoot
 }
